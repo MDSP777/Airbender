352b1800c7b036e579697229a51a984d29c3755a
geloko <angelo92497@yahoo.com>
2016-07-25 17:04:51 +0800

    ...

--- src/model/User.java
+++ src/model/User.java
@@ -1,40 +1,59 @@
+@@ -1,65 +1,40 @@
 package model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class User 
 {
 	private String username;
-	private String password;
-	private int id;
+	private String email;
+	@OneToOne(cascade=CascadeType.ALL)
+	private Address billingAddress;
+	@OneToOne(cascade=CascadeType.ALL)
+	private Address shippingAddress;
 	
-	public User(String username, String password, int id) {
+	public User(String name, String username, String email,
+			Address billingAddress, Address shippingAddress) {
 		super();
+		this.name = name;
 		this.username = username;
-		this.password = password;
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
+		this.email = email;
+		this.billingAddress = billingAddress;
+		this.shippingAddress = shippingAddress;
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public String getName() {
+		return name;
+		this.password = password;
 		this.id = id;
 	}
+
+	public String getEmail() {
+		return email;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Address getBillingAddress() {
+		return billingAddress;
+
+	public Address getShippingAddress() {
+		return shippingAddress;
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", username=" + username
+				+ ", email=" + email + ", billingAddress=" + billingAddress
+				+ ", shippingAddress=" + shippingAddress + "]";
 	
 }
