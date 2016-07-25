d71531070bdd49fd69cc3b964f4499e710f69235
geloko <angelo92497@yahoo.com>
2016-07-25 16:51:37 +0800

    UI

--- src/model/User.java
+++ src/model/User.java
@@ -1,65 +1,40 @@
 package model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
-@Entity
-public class User {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	private String name;
+public class User 
+{
 	private String username;
-	private String email;
-	@OneToOne(cascade=CascadeType.ALL)
-	private Address billingAddress;
-	@OneToOne(cascade=CascadeType.ALL)
-	private Address shippingAddress;
+	private String password;
+	private int id;
 	
-	public User(String name, String username, String email,
-			Address billingAddress, Address shippingAddress) {
+	public User(String username, String password, int id) {
 		super();
-		this.name = name;
 		this.username = username;
-		this.email = email;
-		this.billingAddress = billingAddress;
-		this.shippingAddress = shippingAddress;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
+		this.password = password;
+		this.id = id;
 	}
 
 	public String getUsername() {
 		return username;
 	}
 
-	public String getEmail() {
-		return email;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public Address getBillingAddress() {
-		return billingAddress;
+	public String getPassword() {
+		return password;
 	}
 
-	public Address getShippingAddress() {
-		return shippingAddress;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", username=" + username
-				+ ", email=" + email + ", billingAddress=" + billingAddress
-				+ ", shippingAddress=" + shippingAddress + "]";
+	public int getId() {
+		return id;
 	}
-	
+
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 }
