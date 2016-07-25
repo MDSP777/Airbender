a9b792783cd2f61135a62c3c08e3ed9fff40df0a
MDSP777 <mdsp777@outlook.com>
2016-07-25 12:05:14 +0800

    initial implementation of bcrypt

--- src/model/User.java
+++ src/model/User.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 @Entity
 public class User {
 	@Id
@@ -15,17 +17,21 @@ public class User {
 	private String name;
 	private String username;
 	private String email;
+	private String password;
+	private String salt;
 	@OneToOne(cascade=CascadeType.ALL)
 	private Address billingAddress;
 	@OneToOne(cascade=CascadeType.ALL)
 	private Address shippingAddress;
 	
-	public User(String name, String username, String email,
+	public User(String name, String username, String email, String pw,
 			Address billingAddress, Address shippingAddress) {
 		super();
 		this.name = name;
 		this.username = username;
 		this.email = email;
+		this.salt = BCrypt.gensalt(12);
+		this.password = BCrypt.hashpw(pw, salt);
 		this.billingAddress = billingAddress;
 		this.shippingAddress = shippingAddress;
 	}
--- src/test/AddPostTest.java
+++ src/test/AddPostTest.java
@@ -5,9 +5,11 @@ import java.util.Calendar;
 import model.Address;
 import model.User;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -24,7 +26,16 @@ public class AddPostTest {
 		Address billingAddress = new Address(9, "Glacier Street", "Merville", "Paranaque City", 1700, "Philippines");
 		Address shippingAddress = new Address(2401, "Taft Ave.", "Malate", "Manila", 1234, "Philippines");
 		
-		User u = new User("Marc San Pedro", "MDSP777", "mdsp777@outlook.com", billingAddress, shippingAddress);
+		User u = new User("Marc San Pedro", "MDSP777", "mdsp777@outlook.com", 
+				"helloworld", billingAddress, shippingAddress);
 		userService.addUser(u);
+		User u2 = new User("Marc San Pedro 2", "MDSP7772", "mdsp777@outlook.com", 
+				"goodbyeworld", billingAddress, shippingAddress);
+		userService.addUser(u2);
+		
+		String hash = userService.getHashFor("MDSP777");
+		Assert.assertTrue(BCrypt.checkpw("helloworld", hash));
+		hash = userService. getHashFor("MDSP7772");
+		Assert.assertTrue(BCrypt.checkpw("goodbyeworld", hash));
 	}
 }
